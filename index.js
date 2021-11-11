7c77b0afca55e0ab73e1ee311cc0c670c0a31d02
Steven <steven.bouche@outlook.com>
2021-11-11 21:02:42 +0100

    update

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 
 import Express from 'express'
-import fs from 'fs'
 
 const app = Express()
-const port = 3000
 
-// Cette ligne indique le répertoire qui contient
-// les fichiers statiques: html, css, js, images etc.
+app.set('port', process.env.PORT || 8080);
+
 app.use(Express.static('public'))
-//app.use(Express.static('bundle'))
 app.use(Express.static('dist'));
+
 app.get('/', (req, res) => {
   res.render('index')
 })
